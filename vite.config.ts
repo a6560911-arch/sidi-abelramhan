4b1311f61bcd17f1b3c14dd0b182d9b7aca171af
ahmed <ahmed@gmail.com>
2026-04-05 20:45:42 +0200

    sd

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/a6560911-arch.github.io/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
